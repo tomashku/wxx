9404047b6375de0fe3fdaa45a4bc2f45f53a7f90
tomashku <tzuczek@mac.com>
2022-06-18 18:09:20 +0300

    Initial Commit

---
+++ wx/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  wx
+//
+//  Created by Tomasz Zuczek on 18/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wx/wxApp.swift
@@ -0,0 +1,17 @@
+//
+//  wxApp.swift
+//  wx
+//
+//  Created by Tomasz Zuczek on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct wxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
